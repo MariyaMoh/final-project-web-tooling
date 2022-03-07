b9f66e7800daeeb0b25715a0d5360ae79a1fbd26
MariyaMoh <alshaibi199732@gmail.com>
2022-03-07 15:17:32 +0400

    commit2

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,89 @@ import App from './App';
 import TodoList from './components/TodoList';
 import '@testing-library/jest-dom';
 
+//test1
 test('render h1 element', () => {
   render(<TodoList />);
+  expect(screen.getByTestId('heading')).toBeInTheDocument();
+});
 
-  screen.debug();
+//test2
+export const removeElement = (id, arr) => {
+  return arr.filter((todo) => todo.id !== id);
+};
+const todos = [
+  {
+    id: '1',
+    task: 'take a walk'
+  },
+  {
+    id: '2',
+    task: 'Call Mom'
+  },
+  {
+    id: '3',
+    task: 'prepare for presentation'
+  }
+];
 
-  expect(screen.getByText('Whats the Plan for Today?')).toBeInTheDocument();
+const expected = [
+  {
+    id: '1',
+    task: 'take a walk'
+  },
+  {
+    id: '2',
+    task: 'Call Mom'
+  }
+];
+
+it('it removes an element', () => {
+  expect(removeElement('3', todos)).toMatchObject(expected);
+});
+
+//test3
+export const addNewTodo = (todo, todos) => {
+  if (!todo.text || /^\s*$/.test(todo.text)) {
+    return todos;
+  }
+
+  const newTodos = [todo, ...todos];
+  return newTodos;
+};
+
+describe('Test for add todo', () => {
+  const todo1 = {
+    id: '4',
+    text: 'learn arabic'
+  };
+
+  const todo2 = {
+    id: '5',
+    text: ''
+  };
+  const todos1 = [
+    {
+      id: '1',
+      text: 'take a walk'
+    }
+  ];
+
+  const expected = [
+    {
+      id: '4',
+      text: 'learn arabic'
+    },
+    {
+      id: '1',
+      text: 'take a walk'
+    }
+  ];
+
+  it('returns the updated array', () => {
+    expect(addNewTodo(todo1, todos1)).toMatchObject(expected);
+  });
+
+  it('return the same array', () => {
+    expect(addNewTodo(todo2, todos1)).toMatchObject(todos1);
+  });
 });
